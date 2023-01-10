c2b6e53760f7a2a459453e78e54c2c89e269bf45
LoRiot <lo_riot@riseup.net>
2023-01-11 00:18:05 +0200

    Use enum types for user columns

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from app import db
 
 
 class Gender(Enum):
-    female = 1
-    male = 2
+    female = "female"
+    male = "male"
 
 
 class MaritalStatus(Enum):
@@ -15,8 +15,24 @@ class MaritalStatus(Enum):
 
 
 class User(db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
-    gender = db.Column(db.Enum(Gender))
-    age = db.Column(db.Integer)
+    gender = db.Column(db.Enum("female", "male", name="gender_type"))
+    age = db.Column(db.Enum(
+        "20-24", "25-29", "30-34", "35-39", "40-44",
+        "45-49", "50-54", "55-59", "60-64", "65-69", "> 70",
+        name="age_type"
+    ))
     place_of_residence = db.Column(db.String)
-    martial_status = db.Column()
+    marital_status = db.Column(db.Enum(
+        "Single without children",
+        "Single with children",
+        "Married without children",
+        "Married with children",
+        name="martial_status_type"
+    ))
+    income = db.Column(db.Enum(
+        "above average", "average", "below average",
+        name="income_type"
+    ))
