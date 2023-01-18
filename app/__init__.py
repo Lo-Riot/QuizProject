cb68decf664eb7eb75daf76b472f699aa0493a76
LoRiot <lo_riot@riseup.net>
2023-01-19 00:32:50 +0200

    CORS support and create_admin command

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,31 @@
+import logging
+import click
+
 from flask import Flask
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash
+
 from app.db import db
-from app.views import users_bp
+from app.views.user import users_bp
+from app.views.admin import admin_bp
+
+from app.services import create_admin
 
 
 app = Flask(__name__)
 app.config.from_pyfile("application.cfg")
-CORS(app)
+
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+logging.getLogger('flask_cors').level = logging.DEBUG
 db.init_app(app)
 
 app.register_blueprint(users_bp)
+app.register_blueprint(admin_bp)
+
+
+@app.cli.command("create-admin")
+@click.argument('username', nargs=1)
+@click.argument('password', nargs=1)
+def create_admin_command(username, password):
+    create_admin(username, generate_password_hash(password))
+    db.session.commit()
