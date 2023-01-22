7a0732e63c2866b75736299216163fab364bd5ae
LoRiot <lo_riot@riseup.net>
2023-01-22 17:10:42 +0200

    Create admin panel

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,14 +1,26 @@
 from flask import (
     current_app, Blueprint, request, session,
-    redirect, render_template, send_from_directory
+    redirect, render_template, send_from_directory, jsonify
 )
 from werkzeug.security import check_password_hash
+from app.db import db
 from app.services import (
-    get_admin_by_username, get_admin_by_id, export_statistics
+    get_admin_by_username, get_admin_by_id, export_statistics, delete_tables
 )
 
 
-admin_bp = Blueprint('admin', __name__)
+admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+@admin_bp.route('/panel', methods=["GET"])
+def panel():
+    admin_id = session.get('admin_id')
+    admin = get_admin_by_id(admin_id)
+
+    if admin is not None:
+        return render_template("admin_panel.html")
+    else:
+        return redirect('/admin/login')
 
 
 @admin_bp.route('/login', methods=["GET", "POST"])
@@ -20,7 +32,14 @@ def login():
         admin = get_admin_by_username(username)
         if admin is not None and check_password_hash(admin.password, password):
             session['admin_id'] = admin.id
-            return redirect('/statistics')
+            return redirect('/admin/panel')
+
+    admin_id = session.get('admin_id')
+    admin = get_admin_by_id(admin_id)
+
+    if admin is not None:
+        return redirect('/admin/panel')
+
     return render_template("admin_login.html")
 
 
@@ -35,3 +54,14 @@ def statistics():
         return send_from_directory(
             current_app.config['UPLOAD_FOLDER'], statistics_filename
         )
+
+
+@admin_bp.route('/reset', methods=["GET"])
+def reset_db():
+    admin_id = session.get('admin_id')
+    admin = get_admin_by_id(admin_id)
+
+    if admin is not None:
+        delete_tables()
+        db.session.commit()
+        return jsonify({'success': True, 'msg': "Данные из всех таблиц удалены."})
